dbc4a88435408c3aa3eadba701bcaaf0a5262c58
deviant <mb.deviant@gmail.com>
2022-12-22 21:56:56 +0300

    create inital notes function

---
+++ src/notes.js
@@ -0,0 +1,9 @@
+export default function createNote() {
+    // const container = document.getElementById("content-container");
+    const noteButton = document.getElementById("notes-button");
+    // const addButton = document.createElement("button");
+
+    noteButton.addEventListener("click", () => {
+        console.log("haha");
+    });
+}
