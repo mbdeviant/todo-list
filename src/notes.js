5944af05d12da7faed15088f98887a7a9a27e2a8
deviant <mb.deviant@gmail.com>
2022-12-23 03:48:11 +0300

    create inital notes function

--- src/notes.js
+++ src/notes.js
@@ -1,9 +1,54 @@
-export default function createNote() {
-    // const container = document.getElementById("content-container");
-    const noteButton = document.getElementById("notes-button");
-    // const addButton = document.createElement("button");
+export default function createNoteDisplay() {
+    const container = document.getElementById("content-container");
+    const notesButton = document.getElementById("notes-button");
 
-    noteButton.addEventListener("click", () => {
-        console.log("haha");
+    notesButton.addEventListener("click", () => {
+        if (container.hasChildNodes("noteContainer")) return;
+        container.appendChild(createNoteContainer());
     });
+}
+
+function createNoteContainer() {
+    const container = document.getElementById("content-container");
+    const noteContainer = document.createElement("div");
+    noteContainer.setAttribute("id", "note-container");
+
+    const addNoteButton = document.createElement("button");
+    addNoteButton.classList.add("add-note-button");
+    addNoteButton.setAttribute("id", "add-note-button");
+    addNoteButton.innerHTML = "+";
+
+    container.appendChild(addNoteButton);
+
+    addNoteButton.addEventListener("click", () => {
+        noteContainer.appendChild(createNote());
+    });
+
+    return noteContainer;
+}
+
+function createNote() {
+    const noteContainer = document.getElementById("note-container");
+    const noteItem = document.createElement("div");
+    noteItem.classList.add("note-item");
+
+    const noteText = document.createElement("textarea");
+    noteText.classList.add("note-text");
+    noteText.spellcheck = false;
+    noteText.style.backgroundColor = `hsl(${360 * Math.random()},${
+        25 + 70 * Math.random()
+    }%,${85 + 10 * Math.random()}%)`;
+
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("id", "remove-button");
+    removeButton.classList.add("remove-button");
+    removeButton.innerHTML = "X";
+
+    removeButton.addEventListener("click", () => {
+        noteContainer.removeChild(noteItem);
+    });
+    noteItem.appendChild(noteText);
+    noteItem.appendChild(removeButton);
+
+    return noteItem;
 }
