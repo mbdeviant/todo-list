ec54224c870ab9c02c9b3b071610dde12fe150f0
deviant <mb.deviant@gmail.com>
2023-01-11 22:12:57 +0300

    import projects function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import createNoteDisplay from "./notes";
+import createProjectDisplay from "./projects";
 import createTasksDisplay from "./tasks";
 
 createNoteDisplay();
 createTasksDisplay();
+createProjectDisplay();
